1634375402d3d4e4bd88fe4cc99a796b78d2aa3f
Squ1nty <vincentdao288@gmail.com>
2026-08-25 12:28:24 +1000

    feat: add name as main heading in namecard-esque style

--- components/LandingPageNameCard.tsx
+++ components/LandingPageNameCard.tsx
@@ -1,3 +1,10 @@
 export default function NameCard(){
-  return(<div></div>);
+  return(
+    <div className='w-full h-svh flex flex-col font-extrabold items-center'>
+      <h1 className='md:self-start text-[75px] leading-tight'>Vincent</h1>
+      <h1 className='md:self-end text-[150px] leading-tight'>DAO</h1>
+
+      {/* Add portrait image behind */}
+    </div>
+  );
 }
