53b8fe65f09c67e01949f447af9414dfaecf2921
Squ1nty <vincentdao288@gmail.com>
2026-08-25 12:29:54 +1000

    feat: add namecard component to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import LandingPageNameCard from "@/components/LandingPageNameCard";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <div className='px-3'>
+        <LandingPageNameCard />
+      </div>
     </main>
   );
 }
