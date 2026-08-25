f54b88740b418edb6c44d94302f7f7847513f3f1
Squ1nty <vincentdao288@gmail.com>
2026-08-25 12:50:21 +1000

    style: add desktop sizing for text, minor layout changes to center text

--- components/LandingPageNameCard.tsx
+++ components/LandingPageNameCard.tsx
@@ -1,8 +1,8 @@
 export default function NameCard(){
   return(
-    <div className='w-full h-svh flex flex-col font-extrabold items-center'>
-      <h1 className='md:self-start text-[75px] leading-tight'>Vincent</h1>
-      <h1 className='md:self-end text-[150px] leading-tight'>DAO</h1>
+    <div className='w-full h-full flex flex-col font-extrabold items-center md:justify-center'>
+      <h1 className='text-[75px] leading-tight md:text-[100px]'>Vincent</h1>
+      <h1 className='text-[150px] leading-tight md:text-[225px]'>DAO</h1>
 
       {/* Add portrait image behind */}
     </div>
