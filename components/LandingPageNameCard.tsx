d9aaf1412f369efca4f18ec3202f75bda16c21de
Squ1nty <vincentdao288@gmail.com>
2026-08-26 11:21:56 +1000

    style: change text styling to begin reponsive changes at sm instead of md

--- components/LandingPageNameCard.tsx
+++ components/LandingPageNameCard.tsx
@@ -1,8 +1,8 @@
 export default function NameCard(){
   return(
     <div className='w-full h-full flex flex-col font-extrabold items-center justify-center'>
-      <h1 className='text-[65px] leading-tight md:text-[100px] tracking-tight md:tracking-[25px] lg:tracking-[35px]'>Vincent</h1>
-      <h1 className='text-[125px] leading-tight md:text-[225px]'>DAO</h1>
+      <h1 className='text-[65px] leading-tight sm:text-[100px] tracking-tight sm:tracking-[25px] lg:tracking-[35px]'>Vincent</h1>
+      <h1 className='text-[125px] leading-tight sm:text-[225px]'>DAO</h1>
 
       {/* Add portrait image behind */}
     </div>
