9a219c9b552daa110ebd449da0df8bddb7143b9c
Squ1nty <vincentdao288@gmail.com>
2026-09-09 11:15:41 +1000

    feat: update page.tsx to include Bio.tsx component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import LandingPageNameCard from "@/components/LandingPageNameCard";
 import Projects from "@/components/Projects";
 import Navbar from "@/components/Navbar";
+import Bio from "@/components/Bio";
 
 export default function Home() {
   return (
@@ -9,7 +10,9 @@ export default function Home() {
       <section id="home" className="h-svh px-3">
         <LandingPageNameCard />
       </section>
-
+      <section id="about">
+        <Bio />
+      </section>
       <section id="projects">
         <Projects />
       </section>
