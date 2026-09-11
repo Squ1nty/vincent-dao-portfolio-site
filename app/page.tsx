675c9a62c280feaeb64751dc4279182ab35f2a64
Vincent Dao <vincentdao288@gmail.com>
2026-09-11 11:11:53 +1000

    feat: add adventure path component to app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,22 @@ import LandingPageNameCard from "@/components/LandingPageNameCard";
 import Projects from "@/components/Projects";
 import Navbar from "@/components/Navbar";
 import Bio from "@/components/Bio";
+import AdventurePath from "@/components/AdventurePath";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
-      <section id="home" className="h-svh px-3">
-        <LandingPageNameCard />
-      </section>
-      <section id="about">
-        <Bio />
-      </section>
-      <section id="projects">
-        <Projects />
-      </section>
+      <AdventurePath>
+        <section id="home" className="h-svh px-3">
+          <LandingPageNameCard />
+        </section>
+        <section id="about">
+          <Bio />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+      </AdventurePath>
     </main>
   );
 }
